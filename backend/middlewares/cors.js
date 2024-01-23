e22e3ec23ac1aba12fa950b8f5058d5228060288
Saigin Sergei <Saigin9813@yandex.ru>
2024-01-23 17:21:35 +0500

    add frontend react-auth

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -10,14 +10,14 @@ const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 module.exports = (req, res, next) => {
   const { origin } = req.headers;
   const { method } = req;
-  const requestHeaders = req.headers['access-control-request-headers'];
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
   }
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    res.end();
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+   return res.end();
   }
-  next();
+   return next();
 };
