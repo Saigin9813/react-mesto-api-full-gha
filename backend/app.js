d165b3fae003f099a42c75bdbf3fe6db98489b0a
Saigin Sergei <Saigin9813@yandex.ru>
2024-01-23 19:07:25 +0500

    add frontend react-auth

--- backend/app.js
+++ backend/app.js
@@ -32,12 +32,13 @@ const allowedCors = [
 
 // eslint-disable-next-line consistent-return
 app.use((req, res, next) => {
-  const { origin } = req.headers;
+  const { origin } = req.headers.origin;
   const { method } = req;
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   const requestHeaders = req.headers['access-control-request-headers'];
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
     res.header('Access-Control-Allow-Credentials', true);
   }
   if (method === 'OPTIONS') {
